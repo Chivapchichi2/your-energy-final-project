38d5d2c804180ecb9635ff64ee1d513ea8eb5317
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2023-12-13 20:03:29 +0200

    clear main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { ActionNames } from './js/misc/names.js';
-import apiManager from './js/service/apiManager.js';
-
-apiManager.getExercisesByFilters('pull', 'back', 'bodypart');
 
 /**
  * Test Js and example import
