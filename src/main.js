5ddcfeb888c760b17f74592a3e3570002712ad65
Iryna Samsonenko <samsonenko.iryna@gmail.com>
2023-12-15 06:25:42 +0200

    quote added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { ActionNames } from './js/misc/names.js';
 import { burgerButton } from './js/burger.js';
 import { filterBtnsListeners } from './js/filter/filter.js';
+import { loadQuote } from './js/quote/quote.js';
 
 burgerButton();
 filterBtnsListeners();
+document.addEventListener('DOMContentLoaded', function () {
+  loadQuote();
+});
 /**
  * Test Js and example import
  */
