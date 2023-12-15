1c6c5a6581fe8eba52d8816b23ca963d2fdc0ab7
Olena <33802160+bilcha@users.noreply.github.com>
2023-12-14 21:13:28 -0800

    add email input validation, add listener and handlers for submit button in footer,  fix styles

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { ActionNames } from './js/misc/names.js';
 import { burgerButton } from './js/burger.js';
 import { filterBtnsListeners } from './js/filter/filter.js';
+import { Footer } from './js/footer.js'
+
 
 burgerButton();
 filterBtnsListeners();
+Footer.init();
 /**
  * Test Js and example import
  */
