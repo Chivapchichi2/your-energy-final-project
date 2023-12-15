571c6b910530b1e2a5d0aa79b7f79e0ac0bbf38d
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2023-12-15 11:27:06 +0200

    CSS fix variables duplicate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { burgerButton } from './js/burger.js';
 import { filterBtnsListeners } from './js/filter/filter.js';
-import { Footer } from './js/footer.js'
+import { Footer } from './js/footer.js';
 import { loadQuote } from './js/quote/quote.js';
+import { renderCards } from './js/tamplates/cardTmp.js';
+import apiManager from './js/service/apiManager.js';
 
 burgerButton();
 filterBtnsListeners();
@@ -9,3 +11,5 @@ Footer.init();
 document.addEventListener('DOMContentLoaded', function () {
   loadQuote();
 });
+
+renderCards(apiManager.getExercisesByFilters('', 'waist', 'bodypart'));
