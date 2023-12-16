a2d9e1ce88cf96a94aa2a4b5cf15cf515b5f0d9e
Ruslan <ruslan.grudistov@gmail.com>
2023-12-16 09:21:07 +0200

    pagination

--- src/js/service/pagination.js
+++ src/js/service/pagination.js
@@ -1,68 +1,69 @@
 import { APIClient } from './apiClient';
 
-console.log('PaGINATION');
+async function pagination(filter) {
+  function render(data) {
+    return `${result.data.page} ${result.data.results.map(({ name }) => name)}`;
+  }
 
-const content = document.querySelector('.content');
-const test = document.querySelector('.test');
-let currentPage = 1;
+  const content = document.querySelector('.content');
+  const container = document.querySelector('.test');
+  let currentPage = 1;
 
-const apiClient = new APIClient();
-async function api(page) {
-  return await apiClient.fetchFiltersOfExercises(page);
-}
-let result = await api(currentPage);
-test.innerHTML = `${result.data.page} ${result.data.results.map(
-  ({ name }) => name
-)}`;
-console.log('result', result.data);
-const { perPage, page, totalPages } = result.data;
+  const apiClient = new APIClient();
+  async function api(filter, page = 1) {
+    return await apiClient.fetchFiltersOfExercises(filter, page);
+  }
+  let result = await api(filter, currentPage);
+  container.innerHTML = render(result);
+  console.log('result', result.data);
+  const { perPage, page, totalPages } = result.data;
 
-function pagination() {
-  createPageButtons();
-  showPage(currentPage);
-}
+  function pagination() {
+    createPageButtons();
+    showPage(currentPage);
+  }
 
-function updateActiveButtonStates() {
-  const pageButtons = document.querySelectorAll('.pagination button');
-  pageButtons.forEach((button, index) => {
-    if (index === currentPage - 1) {
-      button.classList.add('active');
-    } else {
-      button.classList.remove('active');
-    }
-  });
-}
-
-function showPage() {
-  updateActiveButtonStates();
-}
-
-// create buttons
-function createPageButtons() {
-  const paginationContainer = document.createElement('div');
-  const paginationDiv = document.body.appendChild(paginationContainer);
-  paginationContainer.classList.add('pagination');
-
-  // Add page buttons
-  for (let i = 0; i < totalPages; i++) {
-    const pageButton = document.createElement('button');
-    pageButton.textContent = i + 1;
-    pageButton.addEventListener('click', async () => {
-      currentPage = i + 1;
-      // showPage(currentPage);
-
-      if (currentPage !== parseInt(result.data.page)) {
-        result = await api(currentPage);
-        updateActiveButtonStates();
-        console.log(result.data);
-        test.innerHTML = `${result.data.page} ${result.data.results.map(
-          ({ name }) => name
-        )}`;
+  function updateActiveButtonStates() {
+    const pageButtons = document.querySelectorAll('.pagination button');
+    pageButtons.forEach((button, index) => {
+      if (index === currentPage - 1) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
       }
     });
+  }
 
-    content.appendChild(paginationContainer);
-    paginationDiv.appendChild(pageButton);
+  function showPage() {
+    updateActiveButtonStates();
+  }
+
+  function createPageButtons() {
+    const paginationContainer = document.createElement('div');
+    const paginationDiv = document.body.appendChild(paginationContainer);
+    paginationContainer.classList.add('pagination');
+
+    for (let i = 0; i < totalPages; i++) {
+      const pageButton = document.createElement('button');
+      pageButton.textContent = i + 1;
+      pageButton.addEventListener('click', async () => {
+        currentPage = i + 1;
+
+        if (currentPage !== parseInt(result.data.page)) {
+          result = await api(filter, currentPage);
+          updateActiveButtonStates();
+          console.log(result.data);
+          container.innerHTML = render(result);
+        }
+      });
+
+      content.appendChild(paginationContainer);
+      paginationDiv.appendChild(pageButton);
+    }
+  }
+
+  if (totalPages > 1) {
+    pagination();
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import { ActionNames } from './js/misc/names.js';
 import { burgerButton } from './js/burger.js';
 import { pagination } from './js/service/pagination.js';
 burgerButton();
-pagination();
+
+// let filter = 'Body parts';
+// let filter = 'Muscles';
+let filter = 'Equipment';
+
+pagination(filter);
 /**
  * Test Js and example import
  */
