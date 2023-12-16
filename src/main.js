ef34280a7704d5ef3edf7e93d8083667e2ceafb7
Olena <33802160+bilcha@users.noreply.github.com>
2023-12-15 22:06:02 -0800

    Add scrollToTop btn, add observer to show scroll btn, add functionality for scrollToTop btn

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { burgerButton } from './js/burger.js';
 import { filterBtnsListeners } from './js/filter/filter.js';
 import { Footer } from './js/footer.js'
 import { loadQuote } from './js/quote/quote.js';
+import  setupScroller from './js/scroll.js'
 
 burgerButton();
 filterBtnsListeners();
 Footer.init();
+setupScroller();
 document.addEventListener('DOMContentLoaded', function () {
   loadQuote();
 });
