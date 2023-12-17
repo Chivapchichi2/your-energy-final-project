c0913801d11e60b84d440c6922fe6d0e54840425
Max Samsonenko <samsonenko.maxwork@gmail.com>
2023-12-17 15:36:10 +0200

    favorites styles complete

--- src/js/favorites/renderFavorites.js
+++ src/js/favorites/renderFavorites.js
@@ -6,85 +6,8 @@ const emptyStorageMsg = document.querySelector('.favorites-error-message');
 
 const exercise = new Exercise(favoritesList, true);
 
-const exercisesData = [
-  {
-    _id: '64f389465ae26083f39b183e',
-    bodyPart: 'back',
-    equipment: 'cable',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0180.gif',
-    name: 'cable low seated row',
-    target: 'upper back',
-    description:
-      'This region includes several muscles like the rhomboids and rear deltoids, responsible for scapular retraction and shoulder external rotation. Exercises include rows and face pulls.',
-    rating: 4.17,
-    burnedCalories: 284,
-    time: 3,
-    popularity: 1343,
-  },
-  {
-    _id: '64f389465ae26083f39b183e',
-    bodyPart: 'back',
-    equipment: 'cable',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0180.gif',
-    name: 'cable low seated row',
-    target: 'upper back',
-    description:
-      'This region includes several muscles like the rhomboids and rear deltoids, responsible for scapular retraction and shoulder external rotation. Exercises include rows and face pulls.',
-    rating: 4.17,
-    burnedCalories: 284,
-    time: 3,
-    popularity: 1343,
-  },
-  {
-    _id: '64f389465ae26083f39b183e',
-    bodyPart: 'back',
-    equipment: 'cable',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0180.gif',
-    name: 'cable low seated row',
-    target: 'upper back',
-    description:
-      'This region includes several muscles like the rhomboids and rear deltoids, responsible for scapular retraction and shoulder external rotation. Exercises include rows and face pulls.',
-    rating: 4.17,
-    burnedCalories: 284,
-    time: 3,
-    popularity: 1343,
-  },
-  {
-    _id: '64f389465ae26083f39b183e',
-    bodyPart: 'back',
-    equipment: 'cable',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0180.gif',
-    name: 'cable low seated row',
-    target: 'upper back',
-    description:
-      'This region includes several muscles like the rhomboids and rear deltoids, responsible for scapular retraction and shoulder external rotation. Exercises include rows and face pulls.',
-    rating: 4.17,
-    burnedCalories: 284,
-    time: 3,
-    popularity: 1343,
-  },
-  {
-    _id: '64f389465ae26083f39b183e',
-    bodyPart: 'back',
-    equipment: 'cable',
-    gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0180.gif',
-    name: 'cable low seated row',
-    target: 'upper back',
-    description:
-      'This region includes several muscles like the rhomboids and rear deltoids, responsible for scapular retraction and shoulder external rotation. Exercises include rows and face pulls.',
-    rating: 4.17,
-    burnedCalories: 284,
-    time: 3,
-    popularity: 1343,
-  },
-];
-
-const data = JSON.stringify(exercisesData);
-
-localStorage.setItem('exercisesData', data);
-
 function renderFavorites() {
-  const localData = JSON.parse(localStorage.getItem('exercisesData'));
+  const localData = JSON.parse(localStorage.getItem('favorites'));
   console.log(localData);
 
   if (!localData) {
