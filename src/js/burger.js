142686228bb8492bdd41eaec3a5844f2114479c8
Ruslan <ruslan.grudistov@gmail.com>
2023-12-17 21:36:44 +0200

    fix header

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,10 +1,12 @@
 function burgerButton() {
   const burger = document.querySelector('.burger');
   const mobileMenu = document.querySelector('.nav-bar-mobile');
+  const body = document.querySelector('body');
 
   burger.addEventListener('click', function changeBurger() {
     burger.classList.toggle('change');
     mobileMenu.classList.toggle('is-hidden');
+    body.classList.toggle('overlow');
   });
 }
 
